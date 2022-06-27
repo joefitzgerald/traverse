1b1f3ba96c464d2bb0d1c789a87bf9a0ad8cfdeb
Mik Freedman <github@michael-freedman.com>
2022-06-27 11:30:00 -0400

    update kong cli structs to use prefix for ldap connection parameters

--- command/find.go
+++ command/find.go
@@ -8,10 +8,12 @@ import (
 
 type LDAP struct {
 	Email      string `name:"email" arg:"" help:"The person's email address."`
-	URL        string `env:"LDAP_URL" required:"true" help:"The URL of the LDAP server. E.g. ldap.example.com:636."`
-	User       string `env:"LDAP_USER" required:"true" help:"The username for connecting to the LDAP server."`
-	Password   string `env:"LDAP_PASSWORD" required:"true" help:"The password for connecting to the LDAP server."`
-	SearchBase string `env:"LDAP_SEARCH_BASE" optional:"true" help:"The base DN to search for the user. E.g. ou=people,dc=example,dc=com."`
+	Connection struct {
+		URL        string `env:"URL" required:"true" help:"The URL of the LDAP server. E.g. ldap.example.com:636."`
+		User       string `env:"USER" required:"true" help:"The username for connecting to the LDAP server."`
+		Password   string `env:"PASSWORD" required:"true" help:"The password for connecting to the LDAP server."`
+		SearchBase string `env:"SEARCH_BASE" optional:"true" help:"The base DN to search for the user. E.g. ou=people,dc=example,dc=com."`
+	} `embed:"" prefix:"ldap-" envprefix:"LDAP_"`
 }
 
 type Find struct {
@@ -19,7 +21,7 @@ type Find struct {
 }
 
 func (l *LDAP) Run() error {
-	finder := ldap.New(l.URL, l.User, l.Password, l.SearchBase)
+	finder := ldap.New(l.Connection.URL, l.Connection.User, l.Connection.Password, l.Connection.SearchBase)
 
 	result, err := finder.Find(l.Email)
 	if err != nil {
